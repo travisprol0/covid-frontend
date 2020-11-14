ecd4669c774d80689cad01b35c76b6c56642448f
travisprol0 <travisjohnprol@gmail.com>
2020-11-14 18:48:51 -0500

    working on averages

--- src/components/USaverage.js
+++ src/components/USaverage.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react"
 
-function USaverage() {
-    return(
-        <h1></h1>
+class USaverage extends React.Component {
+  state = { data: [] }
+
+  componentDidMount() {
+    fetch("https://api.covidtracking.com/v1/us/daily.json")
+      .then((response) => response.json())
+      .then((data) => this.filterData(data))
+        
+  }
+
+  filterData = (data) => {
+    let sevenDays = data.slice(0, 7)
+    this.setState({data: sevenDays})
+    console.log(sevenDays)
+  }
+
+  newPositives = () => {
+    let positives = this.state.data.map((days) => days.positive)
+    console.log(positives)
+    if (positives !== []){
+        let sum = positives.reduce((a, b) => a + b)
+
+        console.log(sum)
+    }
+
+
+
+  }
+
+  render() {
+    return (
+      <div className="USaverageData">
+        <h5>7 Day Average: </h5>
+        <p>New Positives: {this.newPositives()}</p>
+        <p>New Negatives: </p>
+        <p>Percent Positive: </p>
+        <p>Currently Hospitalized: </p>
+        <p>Hospitalization Increase: </p>
+        <p>Currently in ICU: </p>
+        <p>Currently on Ventilator: </p>
+        <p>Lives Lost: </p>
+      </div>
     )
+  }
 }
 
-export default USaverage; 
+export default USaverage
--- src/components/UStotal.js
+++ src/components/UStotal.js
@@ -4,10 +4,10 @@ function UStotal(props) {
     return(
         <div className="UStotalData">
             <h5>Totals Since 01/22/2020</h5>
-            <p>Total Cases: {props.data.positive.toLocaleString("en")}</p>
+            {/* <p>Total Cases: {props.data.positive.toLocaleString("en")}</p>
             <p>Total Hospitalizations: {props.data.hospitalizedCumulative.toLocaleString("en")}</p>
             <p>Total ICU Patients: {props.data.inIcuCumulative.toLocaleString("en")}</p>
-            <p>Lives Lost: {props.data.death.toLocaleString("en")}</p>
+            <p>Lives Lost: {props.data.death.toLocaleString("en")}</p> */}
         </div>
     )
 }
