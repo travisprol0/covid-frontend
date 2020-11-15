4cb61a56c3f6faa4ed64c912c578849d1987137b
travisprol0 <travisjohnprol@gmail.com>
2020-11-14 19:23:45 -0500

    partial 7 day us averages

--- src/components/USaverage.js
+++ src/components/USaverage.js
@@ -1,40 +1,54 @@
 import React from "react"
 
 class USaverage extends React.Component {
-  state = { data: [] }
+  state = { 
+      newCases: null,
+      newNegatives: null,
+      currentlyHospitalized: null,
+      hospitalizationIncrease: null,
+      dead: null
+   }
 
   componentDidMount() {
     fetch("https://api.covidtracking.com/v1/us/daily.json")
       .then((response) => response.json())
-      .then((data) => this.filterData(data))
+      .then((data) => this.positiveAverage(data))
         
   }
 
-  filterData = (data) => {
+  positiveAverage = (data) => {
     let sevenDays = data.slice(0, 7)
-    this.setState({data: sevenDays})
-    console.log(sevenDays)
+    let positives = sevenDays.map((days) => days.positive)
+    let sum = positives.reduce((a, b) => a + b)
+    let positiveAveragePreDecimal = sum / 7
+    let positiveAveragePreComma = Math.round(positiveAveragePreDecimal)
+    let finalAverage = positiveAveragePreComma.toLocaleString('en')
+    this.setState({newCases: finalAverage})
+    this.negativeAverage(data)
   }
 
-  newPositives = () => {
-    let positives = this.state.data.map((days) => days.positive)
-    console.log(positives)
-    if (positives !== []){
-        let sum = positives.reduce((a, b) => a + b)
+  negativeAverage = (data) => {
+    let sevenDays = data.slice(0, 7)
+    let negatives = sevenDays.map((days) => days.negative)
+    let sum = negatives.reduce((a, b) => a + b)
+    let negativeAveragePreDecimal = sum / 7
+    let negativeAveragePreComma = Math.round(negativeAveragePreDecimal)
+    let finalAverage = negativeAveragePreComma.toLocaleString('en')
+    this.setState({newNegatives: finalAverage})
+    this.percentPositiveAverage(data)
+  }
 
-        console.log(sum)
-    }
-
-
-
+  percentPositiveAverage = (data) => {
+      let percent = (this.state.newCases / this.state.newNegatives) * 100
+      console.log((this.state.newCases / this.state.newNegatives))
   }
 
   render() {
     return (
       <div className="USaverageData">
         <h5>7 Day Average: </h5>
-        <p>New Positives: {this.newPositives()}</p>
-        <p>New Negatives: </p>
+        <p>New Positives: {this.state.newCases}</p>
+        <p>New Negatives: {this.state.newNegatives}</p>
         <p>Percent Positive: </p>
         <p>Currently Hospitalized: </p>
         <p>Hospitalization Increase: </p>
