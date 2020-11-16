21f713f97845de560794de336870bca7514f04ad
travisprol0 <travisjohnprol@gmail.com>
2020-11-16 10:13:21 -0500

    state list component rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './CSS/App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Home  from './containers/Home'
+import StateList from './containers/StateList'
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path = '/' component={Home}/>
+        <Route exact path = '/states' component={StateList}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/containers/State.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../CSS/State.css"
+
+const State = () => {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+export default State
