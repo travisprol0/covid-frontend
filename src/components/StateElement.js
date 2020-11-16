2d92a237f0b0ec7e574cc096bfb1149a1e88a971
travisprol0 <travisjohnprol@gmail.com>
2020-11-16 18:34:08 -0500

    state name rendering

--- src/components/StateElement.js
+++ src/components/StateElement.js
@@ -1,9 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+// const StateElement = (props) => {
+//   return (
+//     <Link to={"/states/" + props.state.id}>
+//     <li>{props.state.state}</li>
+//     </Link>
+
+//   )
+// }
 const StateElement = (props) => {
   return (
-    <Link to={"/states/" + props.state.id}>
+    <Link to={{
+        pathname: "/states/" + props.state.id,
+        state: {state: props}
+        }}>
     <li>{props.state.state}</li>
     </Link>
 
--- src/containers/State.js
+++ src/containers/State.js
@@ -1,10 +1,23 @@
 import React from "react"
 import "../CSS/State.css"
 
-const State = () => {
-  return (
-    <h1>Hello World</h1>
-  )
+class State extends React.Component {
+  state = {
+    id: null,
+    state: null,
+    abbreviation: null,
+  }
+  componentDidMount() {
+    this.setState({
+      id: this.props.location.state.state.state.id,
+      state: this.props.location.state.state.state.state,
+      abbreviation: this.props.location.state.state.state.abbreviation,
+    })
+  }
+
+  render() {
+    return <h1>{this.state.state}</h1>
+  }
 }
 
 export default State
