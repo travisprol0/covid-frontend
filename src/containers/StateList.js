215d02fb179b9c9b5ab521e48b18532060d4ab4d
travisprol0 <travisjohnprol@gmail.com>
2020-11-18 17:19:10 -0500

    dynamic search

--- src/containers/StateList.js
+++ src/containers/StateList.js
@@ -6,50 +6,41 @@ import StateElement from "../components/StateElement"
 class StateList extends React.Component {
   state = {
     states: [],
-    searchTerm: "",
-    filteredStates: [],
+    searchTerm: ""
   }
+
   componentDidMount() {
     fetch("http://localhost:3000/locations")
       .then((response) => response.json())
       .then((data) => this.setState({ states: data }))
   }
 
-  filterStates = (searchTerm) => {
-    if (this.state.data !== null) {
-      let newSearchTerm = searchTerm.toUpperCase()
+  mapStates = () => {
+    let newSearchTerm = this.state.searchTerm.toUpperCase()
+    if (this.state.states !== null && newSearchTerm.length === 0) {
+      return this.state.states.map((state) => (
+        <StateElement key={state.id} state={state} />
+      ))
+    } else if (newSearchTerm.length !== 0) {
+      console.log(newSearchTerm)
       let filteredStates = this.state.states.filter(
         (state) =>
           state.abbreviation.includes(newSearchTerm) ||
           state.state.toUpperCase().includes(newSearchTerm)
       )
-      this.setState({ filteredStates: filteredStates })
-    }
-  }
-
-  mapStates = () => {
-    if (this.state.states !== null && this.state.filteredStates.length === 0) {
-      return this.state.states.map((state) => (
-        <StateElement key={state.id} state={state} />
-      ))
-    } else if (this.state.filteredStates.length !== 0) {
-      return this.state.filteredStates.map((state) => (
+      return filteredStates.map((state) => (
         <StateElement key={state.id} state={state} />
       ))
     }
-  }
-
-  submitHandler = (e) => {
-    e.preventDefault()
-    this.filterStates(this.state.searchTerm)
   }
 
   changeHandler = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
+    this.setState({ searchTerm: e.target.value })
+    this.mapStates()
   }
 
-  resetSearch = () => {
-    this.setState({ filteredStates: [], searchTerm: "" })
+  resetSearch = (e) => {
+    e.preventDefault()
   }
 
   render() {
@@ -59,13 +50,12 @@ class StateList extends React.Component {
         <form onSubmit={this.submitHandler}>
           <label for="search">Search:</label>
           <input
-            id="search"
+            type="text"
             name="searchTerm"
             value={this.state.searchTerm}
             onChange={this.changeHandler}
             placeholder="state or abbreviation"
           ></input>
-          <button>search</button>
           <button onClick={this.resetSearch}>reset</button>
         </form>
         <div>
